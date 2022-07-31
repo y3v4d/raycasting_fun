7fe3967168c5af59f436ec57eeb68abdf9ce54f2
y3v4d <y3v4d@proton.me>
2022-07-31 20:43:51 +0200

    updated follia to newest version (with fixed texture color reading), added texture mapping to walls (not complete)

--- src/main.c
+++ src/main.c
@@ -30,8 +30,8 @@ uint8_t map[64] = {
     1, 0, 0, 0, 0, 0, 0, 3,
     3, 0, 0, 2, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 3,
-    3, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 3,
+    3, 0, 3, 1, 0, 0, 0, 1,
+    1, 0, 0, 3, 0, 0, 0, 3,
     3, 1, 3, 1, 3, 1, 3, 1
 };
 
@@ -39,18 +39,49 @@ vec2f_t mouse = { 0 };
 char column_info[64] = "C: - D: -";
 const float grid_size = 32;
 
+FL_Texture *texture;
+
+uint32_t make_color(int r, int g, int b) {
+    return (uint32_t)((uint8_t)r << 16 | (uint8_t)g << 8 | (uint8_t)b);
+}
+
 void draw_column(int column, float distance, int map_x, int map_y) {
     if(column == mouse.x) {
         snprintf(column_info, 64, "C: %d D: %f", column, distance);
     }
 
-    const uint32_t color = map[map_y * map_width + map_x] == 1 ? 0xffff00 : 0xff00ff;
+    const uint32_t shade = min(255.f / absf(distance) * 2, 255);
+    const uint32_t color = map[map_y * map_width + map_x] == 1 ? make_color(shade, shade, 0) : make_color(shade, 0, shade);
     
     const float fish = (float)column / FL_GetWindowWidth() * fov - fov / 2;
-    const float cdistance = distance * cos(fish * PI / 180);
+    const float cdistance = absf(distance * cos(fish * PI / 180));
     const float half_height = (float)FL_GetWindowHeight() / cdistance / 2;
 
-    FL_DrawLine(column, (float)FL_GetWindowHeight() / 2 - half_height, column, (float)FL_GetWindowHeight() / 2 + half_height, color);
+    FL_DrawLine(column, (FL_GetWindowHeight() >> 1) - half_height, column, (FL_GetWindowHeight() >> 1) + half_height, color);
+}
+
+void draw_textured_column(int column, int offset, float distance, int map_x, int map_y) {
+    if(column == mouse.x) {
+        snprintf(column_info, 64, "C: %d D: %f Offset: %d", column, distance, offset);
+    }
+    
+    const float fish = (float)column / FL_GetWindowWidth() * fov - fov / 2;
+    const float cdistance = absf(distance * cos(fish * PI / 180));
+    const float height = (float)FL_GetWindowHeight() / cdistance;
+    const float half_height = height / 2;
+
+    const float tex_step = grid_size / height;
+    float tex_current = 0;
+
+    uint32_t *p = texture->data + offset;
+
+    for(int i = (FL_GetWindowHeight() >> 1) - half_height; i < (FL_GetWindowHeight() >> 1) + half_height; ++i) {
+        if(i < 0 || i >= FL_GetWindowHeight()) continue;
+
+        FL_DrawPoint(column, i, *(p + (int)(floor(tex_current) * texture->width)));
+
+        tex_current += tex_step;
+    }
 }
 
 float tan_table[360];
@@ -67,8 +98,13 @@ int main() {
         exit(-1);
     }
 
-    // 270 is inf 180 is 0, 90 is inf 0 is 0
+    texture = FL_LoadTexture("data/texture.bmp");
+    if(!texture) {
+        FL_Close();
+        exit(-1);
+    }
 
+    // 270 is inf 180 is 0, 90 is inf 0 is 0
     for(int i = 0; i < 360; ++i) {
         if(i == 270 || i == 90) {
             tan_table[i] = 57; 
@@ -128,7 +164,7 @@ int main() {
         const float dt = timer.delta;
         FL_StartTimer(&timer);
 
-        // basic physics
+        // movement
         player.angle += player.vangle * dt;
         if(player.angle < 0) player.angle += 360.f;
         else if(player.angle > 360.f) player.angle -= 360.f;
@@ -141,7 +177,7 @@ int main() {
 
         snprintf(player_pos_text, 32, "X: %.2f Y: %.2f A: %.2f", player.x, player.y, player.angle);
 
-        // setup fps on-screen text
+        // display fps on screen
         FL_StopTimer(&fps_timer);
         if(fps_timer.delta >= 500) {
             snprintf(fps_text, 16, "FPS: %.2f", 1000.f / timer.delta);
@@ -150,7 +186,8 @@ int main() {
         }
       
         FL_ClearScreen();
-        FL_DrawRect(0, 0, FL_GetWindowWidth(), FL_GetWindowHeight() / 2, 0x444444, true); // ceiling
+         // ceiling
+        FL_DrawRect(0, 0, FL_GetWindowWidth(), FL_GetWindowHeight() / 2, 0x444444, true);
 
         // ==== DRAW GAME ====
         const int mm_values_max = 1024;
@@ -161,12 +198,13 @@ int main() {
         const int WALL_HEIGHT = 1;
         const float step = fov / FL_GetWindowWidth();
 
-        float current = player.angle - fov / 2;
+        float current = round(player.angle) - fov / 2;
         if(current < 0) current += 360.f;
 
         for(int i = 0; i < FL_GetWindowWidth(); i++) {
             int v_map_x = -1, v_map_y = -1, h_map_x = -1, h_map_y = -1;
             float v_wall_distance = 999999.f, h_wall_distance = 999999.f;
+            int v_offset = -1, h_offset = -1;
 
             const float angle_tan = tan(current * PI / 180);
 
@@ -180,17 +218,18 @@ int main() {
                 float Ax = player.x + (player.y - Ay) / -angle_tan;
 
                 for(int j = 0; j < r; ++j) {
-                    uint8_t c = 255.f;
-
                     const float distance = absf(player.y - Ay) / sin(current * PI / 180);
 
                     int map_x = floor(Ax / grid_size), map_y = floor(Ay / grid_size);
-                    if(map_x >= 0 && map_x < map_width && map_y >= 0 && map_y < map_height && map[map_y * map_width + map_x] != 0) {
+
+                    if(map_x < 0 || map_x >= map_width || map_y < 0 || map_y >= map_height) break; // ray went out of map
+                    if(map[map_y * map_width + map_x] != 0) {
                         if(mm_values_size < mm_values_max) {
                             mm_values[mm_values_size].x = map_x;
                             mm_values[mm_values_size++].y = map_y;
                         }
 
+                        h_offset = absf(Ax - map_x * grid_size);
                         h_wall_distance = distance / grid_size;
                         h_map_x = map_x;
                         h_map_y = map_y;
@@ -213,17 +252,18 @@ int main() {
                 float Ay = player.y - (Ax - player.x) * -angle_tan;
 
                 for(int j = 0; j < r; ++j) {
-                    uint8_t c = 255.f;
-
                     const float distance = absf(player.x - Ax) / cos(current * PI / 180);
 
                     int map_x = floor(Ax / grid_size), map_y = floor(Ay / grid_size);
-                    if(map_x >= 0 && map_x < map_width && map_y >= 0 && map_y < map_height && map[map_y * map_width + map_x] != 0) {
+                    if(map_x < 0 || map_x >= map_width || map_y < 0 || map_y >= map_height) break; // ray went out of map
+
+                    if(map[map_y * map_width + map_x] != 0) {
                         if(mm_values_size < mm_values_max) {
                             mm_values[mm_values_size].x = Ax;
                             mm_values[mm_values_size++].y = Ay;
                         }
 
+                        v_offset = absf(Ay - map_y * grid_size);
                         v_wall_distance = distance / grid_size;
                         v_map_x = map_x;
                         v_map_y = map_y;
@@ -237,9 +277,11 @@ int main() {
             }
 
             if(absf(h_wall_distance) < absf(v_wall_distance) && absf(h_wall_distance) < r) {
-                draw_column(i, h_wall_distance, h_map_x, h_map_y);
+                //draw_column(i, h_wall_distance, h_map_x, h_map_y);
+                draw_textured_column(i, h_offset, h_wall_distance, h_map_x, h_map_y);
             } else if(absf(v_wall_distance) < absf(h_wall_distance) && absf(v_wall_distance) < r) {
-                draw_column(i, v_wall_distance, v_map_x, v_map_y);
+                //draw_column(i, v_wall_distance, v_map_x, v_map_y);
+                draw_textured_column(i, v_offset, v_wall_distance, v_map_x, v_map_y);
             }
 
             current += step;
@@ -290,6 +332,7 @@ int main() {
         FL_Render();
     }
 
+    FL_FreeTexture(texture);
     FL_FreeFontBDF(knxt);
     FL_Close();
 
